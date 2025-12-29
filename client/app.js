82d8b1d8e1f681b4695f58fc06e24709c7b970bf
Sahil Chavan <sahilchavanajara327@gmail.com>
2025-12-30 03:48:14 +0530

    Update UI, fix blur issue, and clean backend configuration

--- client/app.js
+++ client/app.js
@@ -1,62 +1,44 @@
-function getBathValue() {
-  var uiBathrooms = document.getElementsByName("uiBathrooms");
-  for(var i in uiBathrooms) {
-    if(uiBathrooms[i].checked) {
-        return parseInt(i)+1;
-    }
-  }
-  return -1; // Invalid Value
-}
+const locationURL = "http://127.0.0.1:5000/get_location_names";
+const predictURL = "http://127.0.0.1:5000/predict_home_price";
 
-function getBHKValue() {
-  var uiBHK = document.getElementsByName("uiBHK");
-  for(var i in uiBHK) {
-    if(uiBHK[i].checked) {
-        return parseInt(i)+1;
-    }
-  }
-  return -1; // Invalid Value
+window.onload = () => {
+    fetch(locationURL)
+        .then(res => res.json())
+        .then(data => {
+            const select = document.getElementById("uiLocations");
+            select.innerHTML = "";
+            data.locations.forEach(loc => {
+                const option = document.createElement("option");
+                option.value = loc;
+                option.text = loc;
+                select.appendChild(option);
+            });
+        });
+};
+
+function getValue(name) {
+    return document.querySelector(`input[name="${name}"]:checked`).value;
 }
 
 function onClickedEstimatePrice() {
-  console.log("Estimate price button clicked");
-  var sqft = document.getElementById("uiSqft");
-  var bhk = getBHKValue();
-  var bathrooms = getBathValue();
-  var location = document.getElementById("uiLocations");
-  var estPrice = document.getElementById("uiEstimatedPrice");
+    const sqft = document.getElementById("uiSqft").value;
+    const location = document.getElementById("uiLocations").value;
+    const bhk = getValue("uiBHK");
+    const bath = getValue("uiBathrooms");
+    const result = document.getElementById("uiEstimatedPrice");
 
-  // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    const formData = new FormData();
+    formData.append("total_sqft", sqft);
+    formData.append("location", location);
+    formData.append("bhk", bhk);
+    formData.append("bath", bath);
 
-  $.post(url, {
-      total_sqft: parseFloat(sqft.value),
-      bhk: bhk,
-      bath: bathrooms,
-      location: location.value
-  },function(data, status) {
-      console.log(data.estimated_price);
-      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
-      console.log(status);
-  });
+    fetch(predictURL, {
+        method: "POST",
+        body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+        result.innerHTML = `₹ ${data.estimated_price} Lakh`;
+    });
 }
-
-function onPageLoad() {
-  console.log( "document loaded" );
-  // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-  var url = "/api/get_location_names"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
-  $.get(url,function(data, status) {
-      console.log("got response for get_location_names request");
-      if(data) {
-          var locations = data.locations;
-          var uiLocations = document.getElementById("uiLocations");
-          $('#uiLocations').empty();
-          for(var i in locations) {
-              var opt = new Option(locations[i]);
-              $('#uiLocations').append(opt);
-          }
-      }
-  });
-}
-
-window.onload = onPageLoad;
--- server/util.py
+++ server/util.py
@@ -1,11 +1,34 @@
-import pickle
 import json
+import pickle
 import numpy as np
 import os
 
 __locations = None
 __data_columns = None
 __model = None
+
+
+def load_saved_artifacts():
+    print("Loading saved artifacts...")
+    global __data_columns
+    global __locations
+    global __model
+
+    base_dir = os.path.dirname(__file__)
+
+    with open(os.path.join(base_dir, "artifacts", "columns.json"), "r") as f:
+        __data_columns = json.load(f)["data_columns"]
+        __locations = __data_columns[3:]  # first 3 are sqft, bath, bhk
+
+    with open(os.path.join(base_dir, "artifacts", "banglore_home_prices_model.pickle"), "rb") as f:
+        __model = pickle.load(f)
+
+    print("Artifacts loaded successfully")
+
+
+def get_location_names():
+    return __locations
+
 
 def get_estimated_price(location, sqft, bhk, bath):
     try:
@@ -21,43 +44,3 @@ def get_estimated_price(location, sqft, bhk, bath):
         x[loc_index] = 1
 
     return round(__model.predict([x])[0], 2)
-
-
-def load_saved_artifacts():
-    print("loading saved artifacts...start")
-    global __data_columns
-    global __locations
-    global __model
-
-    # Get the directory where this util.py file is located
-    base_dir = os.path.dirname(__file__)
-    artifacts_path = os.path.join(base_dir, "artifacts")
-
-    # Load columns.json
-    with open(os.path.join(artifacts_path, "columns.json"), "r") as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
-
-    # Load model only once
-    if __model is None:
-        with open(os.path.join(artifacts_path, "banglore_home_prices_model.pickle"), "rb") as f:
-            __model = pickle.load(f)
-
-    print("loading saved artifacts...done")
-
-
-def get_location_names():
-    return __locations
-
-
-def get_data_columns():
-    return __data_columns
-
-
-if __name__ == '__main__':
-    load_saved_artifacts()
-    print(get_location_names())
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
-    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
-    print(get_estimated_price('Kalhalli', 1000, 2, 2))  # other location
-    print(get_estimated_price('Ejipura', 1000, 2, 2))   # other location
